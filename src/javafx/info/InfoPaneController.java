42bf4b4ef050e0250db4158dab8eba4fe784ab35
alecalvo03 <ale_calvo03@outlook.com>
2017-05-25 18:26:23 -0600

    Gladiator info

--- src/javafx/info/InfoPaneController.java
+++ src/javafx/info/InfoPaneController.java
@@ -2,8 +2,6 @@ package javafx.info;
 
 import characters.Gladiator;
 import javafx.MainFX;
-import javafx.collections.transformation.FilteredList;
-import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
@@ -20,12 +18,26 @@ public class InfoPaneController implements Initializable{
 
     @FXML private ListView<Gladiator> lvRomans;
     @FXML private ListView<Gladiator> lvGreeks;
-    @FXML private TableView<Gladiator> tbvStats;
-    @FXML private TableColumn<Gladiator,String> tbcSkillName;
-    @FXML private TableColumn<Gladiator,Float> tbcValue;
     @FXML private Button btnRoman;
     @FXML private Button btnGreek;
     @FXML private Label lbName;
+    @FXML private Label lbKind;
+    @FXML private Label lbGender;
+    @FXML private Label lbHStrikeDmg;
+    @FXML private Label lbFStrikeDmg;
+    @FXML private Label lbHStrikeRes;
+    @FXML private Label lbFStrikeRes;
+    @FXML private Label lbArrowRes;
+    @FXML private Label lbTotalRes;
+    @FXML private Label lbStamina;
+    @FXML private Label lbMaxSpeed;
+    @FXML private Label lbTime;
+    @FXML private Label lbScore;
+    @FXML private Label lbSexAttract;
+    @FXML private Label lbBeauty;
+    @FXML private Label lbIntelligence;
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -48,11 +60,6 @@ public class InfoPaneController implements Initializable{
         });
         lvGreeks.setItems(GladiatorLists.getGreekList());
         lvGreeks.setCellFactory(lvRomans.getCellFactory());
-
-        FilteredList<Gladiator> skillNameList = new FilteredList<>(GladiatorLists.getRomanList());
-        SortedList<Gladiator> sortedSkill = new SortedList<>(skillNameList);
-        sortedSkill.comparatorProperty().bind(tbvStats.comparatorProperty());
-        tbvStats.setItems(sortedSkill);
     }
 
     public void openRomanChart() throws IOException {
@@ -72,6 +79,21 @@ public class InfoPaneController implements Initializable{
     }
 
     public void updateStats(Gladiator gladiator){
-
+        lbName.setText(gladiator.getName());
+        lbKind.setText(gladiator.getKind());
+        lbGender.setText(gladiator.getGender());
+        lbHStrikeDmg.setText(String.valueOf(gladiator.getHandStrikeDamage()));
+        lbFStrikeDmg.setText(String.valueOf(gladiator.getFootStrikeDamage()));
+        lbHStrikeRes.setText(String.valueOf(gladiator.getHandStrikeResistance()));
+        lbFStrikeRes.setText(String.valueOf(gladiator.getFootStrikeResistance()));
+        lbArrowRes.setText(String.valueOf(gladiator.getArrowResistance()));
+        lbTotalRes.setText(String.valueOf(gladiator.getTotalResistance()));
+        lbStamina.setText(String.valueOf(gladiator.getStamina()));
+        lbMaxSpeed.setText(String.valueOf(gladiator.getMaxSpeed()));
+        lbTime.setText(String.valueOf(gladiator.getTime()));
+        lbScore.setText(String.valueOf(gladiator.getScore()));
+        lbSexAttract.setText(String.valueOf(gladiator.getSexualAttraction()));
+        lbBeauty.setText(String.valueOf(gladiator.getBeauty()));
+        lbIntelligence.setText(String.valueOf(gladiator.getIntelligence()));
     }
 }
